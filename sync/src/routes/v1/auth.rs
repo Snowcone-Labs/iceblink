39602d26d0caa4227693fdcc002c48759adf323a
Erb3 <49862976+Erb3@users.noreply.github.com>
2024-10-12 22:39:41 +0200

    feat(sync): find and assign jwt

--- sync/src/routes/v1/auth.rs
+++ sync/src/routes/v1/auth.rs
@@ -1,4 +1,8 @@
-use crate::{models::user, server::AppState};
+use crate::{
+    auth,
+    models::{self},
+    server::AppState,
+};
 use axum::{
     extract::{Query, State},
     http::HeaderMap,
@@ -6,6 +10,7 @@ use axum::{
 use reqwest::{header, StatusCode};
 use serde::Deserialize;
 use std::sync::Arc;
+use tracing::warn;
 
 #[derive(Deserialize)]
 pub struct OauthQueryParams {
@@ -18,23 +23,37 @@ pub async fn oauth(
 ) -> (StatusCode, HeaderMap) {
     let code = query.code.to_string();
     let exchange = state.openid.clone().exchange(code).await;
+    let mut headers = HeaderMap::default();
 
     if let Ok(access_token) = exchange {
         let user_info = state.openid.clone().userinfo(access_token).await;
 
         if let Ok(user_data) = user_info {
             println!("Received user data! {:?}", user_data);
+
+            let attemped_user_fetch =
+                models::user::User::get_by_upstream_id(&state.db, user_data.id).await;
+
+            if let Ok(user_fetch) = attemped_user_fetch {
+                if let Some(user) = user_fetch {
+                    let jwt = auth::create_jwt(&user, state.settings.jwt_secret.clone()).await;
+
+                    headers.insert(header::SET_COOKIE, format!("jwt={jwt}").parse().unwrap());
+
+                    (StatusCode::OK, headers)
+                } else {
+                    todo!("create user");
+                }
+            } else {
+                warn!("Database error catched");
+                return (StatusCode::INTERNAL_SERVER_ERROR, headers);
+            }
         } else {
             println!("Failed to get user data: {:?}", user_info.unwrap());
-            return (StatusCode::UNAUTHORIZED, HeaderMap::default());
+            return (StatusCode::UNAUTHORIZED, headers);
         }
-
-        let mut headers = HeaderMap::new();
-        // headers.insert(header::SET_COOKIE, format!("jwt={jwt}").parse().unwrap());
-
-        (StatusCode::OK, headers)
     } else {
         println!("Failed to exchange: {:?}", exchange.unwrap());
-        (StatusCode::UNAUTHORIZED, HeaderMap::default())
+        (StatusCode::UNAUTHORIZED, headers)
     }
 }
