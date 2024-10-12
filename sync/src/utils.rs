7f9bb39ad7f26ec2fe3e2771db5147f0351a5d42
Erb3 <49862976+Erb3@users.noreply.github.com>
2024-10-12 16:12:52 +0200

    tests(sync): add test for id generatro

--- sync/src/utils.rs
+++ sync/src/utils.rs
@@ -3,3 +3,21 @@ use rand::distributions::{Alphanumeric, DistString};
 pub fn generate_id(len: usize) -> String {
     Alphanumeric.sample_string(&mut rand::thread_rng(), len)
 }
+
+#[cfg(test)]
+mod tests {
+    use regex::Regex;
+
+    use super::*;
+
+    #[test]
+    fn test_generate_id() {
+        let regex = Regex::new(r"[a-zA-Z0-9]*").unwrap();
+
+        for x in 0..50 {
+            let id = generate_id(x);
+            assert!(regex.is_match(id.as_str()));
+            assert_eq!(id.len(), x);
+        }
+    }
+}
