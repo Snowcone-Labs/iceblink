cba88f1b80b859e63f113ade5913c50b46f8cfb2
Erb3 <49862976+Erb3@users.noreply.github.com>
2024-11-25 21:37:13 +0100

    feat(app): copy openid example

--- app/app/index.tsx
+++ app/app/index.tsx
@@ -1,6 +1,28 @@
-import { Text, View } from "react-native";
+import {
+  makeRedirectUri,
+  useAuthRequest,
+  useAutoDiscovery,
+} from "expo-auth-session";
+import * as WebBrowser from "expo-web-browser";
+import { Button, Text, View } from "react-native";
+
+WebBrowser.maybeCompleteAuthSession();
 
 export default function Index() {
+  const discovery = useAutoDiscovery("https://pfapi.snowflake.blue");
+
+  // Create and load an auth request
+  const [request, result, promptAsync] = useAuthRequest(
+    {
+      clientId: "cm3c1gn37000c22v77my7iwnv",
+      redirectUri: makeRedirectUri({
+        scheme: "iceblink",
+      }),
+      scopes: ["openid", "profile"],
+    },
+    discovery
+  );
+
   return (
     <View
       style={{
@@ -9,7 +31,9 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Iceblink</Text>
+      <Button title="Login" disabled={!request} onPress={() => promptAsync()} />
+      {result && <Text>{JSON.stringify(result, null, 2)}</Text>}
     </View>
   );
 }
