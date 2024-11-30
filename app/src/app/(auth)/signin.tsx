c0adeb75fd26ec51f4f8dada02d0a0a4a99779fc
Erb3 <49862976+Erb3@users.noreply.github.com>
2024-11-30 16:26:47 +0100

    feat(app): warmup browser before auth

--- app/src/app/(auth)/signin.tsx
+++ app/src/app/(auth)/signin.tsx
@@ -7,11 +7,20 @@ import {
 } from "expo-auth-session";
 import { Link } from "expo-router";
 import * as WebBrowser from "expo-web-browser";
+import { useEffect } from "react";
 import { Text, View } from "react-native";
 
 WebBrowser.maybeCompleteAuthSession();
 
 export default function SigninPage() {
+  useEffect(() => {
+    WebBrowser.warmUpAsync();
+
+    return () => {
+      WebBrowser.coolDownAsync();
+    };
+  }, []);
+
   const discovery = useAutoDiscovery("https://pfapi.snowflake.blue");
   const [request, result, promptAsync] = useAuthRequest(
     {
@@ -25,14 +34,7 @@ export default function SigninPage() {
   );
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-      className="bg-iceblink-bg-dark p-5"
-    >
+    <View className="bg-iceblink-bg-dark p-5 justify-center items-center flex-1">
       <View className="my-auto flex justify-center items-center">
         <Text className="color-iceblink-fg-dark text-5xl">Iceblink</Text>
         <IceblinkLogo size={200} />
--- app/src/app/(auth)/unlock.tsx
+++ app/src/app/(auth)/unlock.tsx
@@ -1,1 +1,9 @@
-export default function UnlockPage() {}
+import { Text, View } from "react-native";
+
+export default function UnlockPage() {
+  return (
+    <View className="bg-iceblink-bg-dark p-5 justify-center items-center flex-1">
+      <Text className="color-iceblink-fg-dark text-5xl">Unlock Vault</Text>
+    </View>
+  );
+}
